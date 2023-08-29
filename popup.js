f76afb42493647f1af8d8b428c7a37d42e0be3dc
Jeetesh Namdeo Rokade <jeeteshnamdeo.rokade@ivanti.com>
2023-08-29 11:45:18 +0530

    cert mgmt

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
-function faviconURL(u) {
-  const url = new URL(chrome.runtime.getURL('/_favicon/'));
-  url.searchParams.set('pageUrl', u); // this encodes the URL as well
-  url.searchParams.set('size', '32');
-  return url.toString();
+function getUserToken() {
+  chrome.enterprise.platformKeys.getTokens(function(tokens) {
+    for (var i = 0; i < tokens.length; i++) {
+      if (tokens[i].id == "user") {
+        console.log(tokens[i]);
+        return;
+      }
+    }
+
+  });
 }
 
-const img = document.createElement('img');
-// chrome-extension://EXTENSION_ID/_favicon/?pageUrl=https%3A%2F%2Fwww.google.com&size=32
-img.src = faviconURL('https://www.google.com');
-document.body.appendChild(img);
+document.getElementById('getUserToken').addEventListener('click', getUserToken);
