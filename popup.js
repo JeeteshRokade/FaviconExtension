d28bd0d2b05ca5af60495bfd212147b552b6db6b
Jeetesh Namdeo Rokade <jeeteshnamdeo.rokade@ivanti.com>
2023-08-30 13:35:35 +0530

    qwqwr4qrww

--- popup.js
+++ popup.js
@@ -1,14 +1,55 @@
-function getUserToken() {
-  alert("sdgdsgdsgdsgsdsafsafasfsafasfsa");
+function getUserToken(callback) {
+  alert("starting token generataion");
   chrome.enterprise.platformKeys.getTokens(function(tokens) {
     for (var i = 0; i < tokens.length; i++) {
       if (tokens[i].id == "user") {
-        alert(tokens[i]);
+        callback(tokens[i]);
         return;
       }
     }
+    callback(undefined);
   });
-  alert("sdgdsgdsgdsgsd");
 }
 
-document.getElementById('addButton').addEventListener('click', getUserToken);
+document.getElementById('addButton').addEventListener('click', getUserToken(generateAndSign));
+
+
+
+
+function generateAndSign(userToken) {
+  var data = new Uint8Array([0, 5, 1, 2, 3, 4, 5, 6]);
+  var algorithm = {
+    name: "RSASSA-PKCS1-v1_5",
+    // RsaHashedKeyGenParams
+    modulusLength: 2048,
+    publicExponent:
+        new Uint8Array([0x01, 0x00, 0x01]),  // Equivalent to 65537
+    hash: {
+      name: "SHA-256",
+    }
+  };
+  var cachedKeyPair;
+  userToken.subtleCrypto.generateKey(algorithm, false, ["sign"])
+    .then(function(keyPair) {
+            cachedKeyPair = keyPair;
+            return userToken.subtleCrypto.exportKey("spki", keyPair.publicKey);
+          },
+          console.log.bind(console))
+    .then(function(publicKeySpki) {
+            // Build the Certification Request using the public key.
+            return userToken.subtleCrypto.sign(
+                {name : "RSASSA-PKCS1-v1_5"}, cachedKeyPair.privateKey, data);
+          },
+          console.log.bind(console))
+    .then(function(signature) {
+              // Complete the Certification Request with |signature|.
+              // Send out the request to the CA, calling back
+              // onClientCertificateReceived.
+          },
+          console.log.bind(console));
+}
+
+function onClientCertificateReceived(userToken, certificate) {
+  chrome.enterprise.platformKeys.importCertificate(userToken.id, certificate);
+}
+
