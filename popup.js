3c1c7a2fdb02919dfac5845a2a2a49bf1934b7fe
Jeetesh Namdeo Rokade <jeeteshnamdeo.rokade@ivanti.com>
2023-09-04 15:56:50 +0530

    asfasfas

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 function getUserToken(callback) {
-  alert("starting token generataion");
   chrome.enterprise.platformKeys.getTokens(function(tokens) {
     for (var i = 0; i < tokens.length; i++) {
       if (tokens[i].id == "user") {
